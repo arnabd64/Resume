6243d2ae8bf2340c5dc1c63629711fe52b8a3519
arnabd64 <arnabdhar32@gmail.com>
2025-01-06 06:49:22 +0000

    Compiled TeX code

---
+++ install_tex_packages.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# Install TeX packages
+tlmgr install \
+    cmbright \
+    enumitem \
+    fontawesome5 \
+    etoolbox \
+    libertine \
+    microtype \
+    paracol \
+    supertabular \
+    titlesec
